10bc979b4debd8dd11b9da2e2a275d5242ac7f25
Jon Klein <jtklein@alaska.edu>
2015-11-22 13:47:00 -0900

    added rssi logs from walking around taku

--- GPSLocalization/rssi_grabber.py
+++ GPSLocalization/rssi_grabber.py
@@ -11,19 +11,23 @@ import serial
 import time
 import csv 
 
-GPS_ADDR = "BC:F5:AC:74:7C:93" 
-GPS_PORT = 2
-MIN_TIME = 2 # seconds
+GPS_ADDR = "08:D4:2B:1A:58:E9" # "BC:F5:AC:74:7C:93"#
+GPS_PORT = 3
+MIN_TIME = 5 # seconds
 
-BLOCKING = True
+BLOCKING = False
 
 # open up serial port for GPS, return pyserial object
 def init_gps(port = GPS_PORT, addr = GPS_ADDR):
+    print('connecting to GPS')
     socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     socket.connect((addr, port))
+    print('connected to GPS')
+    
     return socket 
 
-def init_radio(port = '/dev/ttyACM1'):
+def init_radio(port = 'COM14'):
+    print('connecting to radio..')
     ser = serial.Serial(port, timeout = 1)
     return ser
 
@@ -49,14 +53,28 @@ def get_rssi(ser):
 
 # read in data from GPS, return location from most recent GPGGA 
 def get_location(gps):
-    gps_data = gps.read()
+    gps_data = ''
+    print('reading in location')
+    gps.setblocking(True)
+    while True:
+        try:
+            d = gps.recv(8096)
+            gps.setblocking(False)
+        except IOError:
+            break
+        gps_data += d
+        print d
+        if not d:
+            break
+    
+    gps_data = '\n'.join(gps_data.split('\n')[1:-1]) # :(
+    
     nmea_stream_reader = pynmea2.NMEAStreamReader()
     gps_loc_msg = None
-
-    for msg in streamreader.next(gps_data):
-        print(msg)
-        if len(str(msg)) > 50 and str(msg)[:6] == '$GPGGA':
-            gps_loc_msg = msg
+    
+    for msg in gps_data.split('\n'):
+        if len(str(msg)) > 50 and 'GPGGA' in msg:
+            gps_loc_msg = pynmea2.parse(msg[:-1])
         
     if gps_loc_msg == None:
         return (0, 0, 0)
@@ -66,7 +84,7 @@ def get_location(gps):
    
 if __name__ == '__main__':
     # create log file
-    csvfilename = 'rssi_location_log_{}.csv'.format(int(time.time))
+    csvfilename = 'rssi_location_log_{}.csv'.format(int(time.time()))
     csvfile = open(csvfilename, 'wb') 
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(['time (s)', 'rssi (dBm)', 'longitude (deg)', 'latitude (deg)', 'error (dil)'])
@@ -80,16 +98,18 @@ if __name__ == '__main__':
         if BLOCKING:
             if 'n' in raw_input('take another measurement? [y]/n: '):
                 break
-        location = get_location(gps_file)
+        else:
+            time.sleep(MIN_TIME)
+        location = get_location(gps)
         rssi = get_rssi(msp_serial)
-        time = time.time()
+        sampletime = time.time()
 
         print('location: ' + str(location))
         print('rssi: ' + str(rssi))
 
-        time.sleep(MIN_TIME) 
-        csvwriter.writerow([time, rssi, location[0], location[1], location[2]])
-    
+        csvwriter.writerow([sampletime, rssi, location[0], location[1], location[2]])
+        csvfile.flush()
+        
     # close up
     csvfile.close()
     close_gps(gps)
